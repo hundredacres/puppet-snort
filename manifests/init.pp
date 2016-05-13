fa0a8a6639a9496927d1b7123157ef90a4619499
Matthew Schmitt <matthew.schmitt@riskiq.net>
2016-05-13 11:49:44 -0700

    Adding params for controlling snort config Updated snort.conf template

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,8 +11,21 @@
 #   e.g. "Specify one or more upstream ntp servers as an array."
 #
 class snort (
-  $package_name = $::snort::params::package_name,
-  $service_name = $::snort::params::service_name,
+  $package_name            = $::snort::params::package_name,
+  $service_name            = $::snort::params::service_name,
+  $ip_ranges               = $::snort::params::ip_ranges,
+  $dns_servers             = $::snort::params::dns_servers,
+  $snort_perfprofile       = $::snort::params::snort_perfprofile,
+  $stream_memcap           = $::snort::params::stream_memcap,
+  $stream_prune_log_max    = $::snort::params::stream_prune_log_max,
+  $stream_max_queued_segs  = $::snort::params::stream_max_queued_segs,
+  $stream_max_queued_bytes = $::snort::params::stream_max_queued_bytes,
+  $perfmonitor_pktcnt      = $::snort::params::perfmonitor_pktcnt,
+  $dcerpc2_memcap          = $::snort::params::dcerpc2_memcap,
+  $enable                  = $::snort::params::enable,
+  $ensure                  = $::snort::params::ensure,
+  $norules                 = $::snort::params::norules,
+  $rotation                = $::snort::params::rotation,
 ) inherits ::snort::params {
 
   # validate parameters here
--- manifests/params.pp
+++ manifests/params.pp
@@ -4,6 +4,21 @@
 # It sets variables according to platform.
 #
 class snort::params {
+  $ip_ranges ='any',
+  $dns_servers = '$HOME_NET',
+  $snort_perfprofile = false,
+  $stream_memcap = '8388608',
+  $stream_prune_log_max = '1048576',
+  $stream_max_queued_segs = '2621',
+  $stream_max_queued_bytes = '1048576',
+  $perfmonitor_pktcnt = '10000',
+  $dcerpc2_memcap = '102400',
+  $enable = true,
+  $ensure = running,
+  $barnyard = false,
+  $norules = false,
+  $rotation = '7'
+
   case $::osfamily {
     'Debian': {
       $package_name = 'snort'
