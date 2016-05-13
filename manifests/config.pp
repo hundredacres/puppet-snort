c495cd2e539d5f8addced9bcd22c2d9d57683b58
Matthew Schmitt <matthew.schmitt@riskiq.net>
2016-05-13 12:43:30 -0700

    Added snort.conf to config class Updated snort.conf.erb template to use scope lookup

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,4 +3,11 @@
 # This class is called from snort for service config.
 #
 class snort::config {
+  file { '/etc/snort/snort.conf':
+    ensure  => file,
+    owner   => root,
+    group   => root,
+    content => template('snort/snort.conf.erb'),
+    mode    => '0644',
+  }
 }
